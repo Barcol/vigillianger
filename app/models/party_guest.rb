9add899e575c47b22558ebcfa834b0772d95feb4
Radek Bigaj <r.bigaj@fibcode.com>
2020-10-21 09:57:42 +0200

    delegated retrieving products cost sum to model; changed ifs to database query

--- app/models/party_guest.rb
+++ app/models/party_guest.rb
@@ -14,4 +14,7 @@ class PartyGuest < ApplicationRecord
     ConsumePreference.create(types_id: type.id, party_guests_id: self.id)
   end
 
+  def product_cost_sum_for_type(product_type_id)
+    products.where(type_id: product_type_id).pluck(:price).sum / 100.0
+  end
 end
